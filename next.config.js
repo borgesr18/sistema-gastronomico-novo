71adc95d1acba5fb284851d9175722fd9020853c
borgesr18 <borgesr18@yahoo.com.br>
2025-06-15 09:58:06 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    serverActions: true,
-  },
   typescript: {
-    ignoreBuildErrors: false, // Se estiver em desenvolvimento pode ajustar para true, mas o correto para produção é false
+    ignoreBuildErrors: false,
   },
   eslint: {
     ignoreDuringBuilds: true,

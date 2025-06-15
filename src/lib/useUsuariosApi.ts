fc6435ee730c7256067b62b0cbebf1e3dea8a8c4
borgesr18 <borgesr18@yahoo.com.br>
2025-06-15 10:08:38 -0300

    Update useUsuariosApi.ts

--- src/lib/useUsuariosApi.ts
+++ src/lib/useUsuariosApi.ts
@@ -1,2 +1,9 @@
+export type Usuario = {
+  id: string;
+  nome: string;
+  email: string;
+  role: 'admin' | 'editor' | 'viewer' | 'manager';
+};
+
 export { useUsuarios as useUsuariosApi } from './usuariosService';
 export * from './usuariosService';

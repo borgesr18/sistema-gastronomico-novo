2d440a67650f765c1d3f10df4028b56ccdfb3091
borgesr18 <borgesr18@yahoo.com.br>
2025-06-15 20:54:01 -0300

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 
-declare global {
-  // Garantimos que o Prisma não gere múltiplas instâncias em modo dev
-  var prisma: PrismaClient | undefined;
-}
-
-// Garantir DATABASE_URL para evitar erros em builds locais ou testes
-if (!process.env.DATABASE_URL) {
-  process.env.DATABASE_URL = 'file:./dev.db';
-}
-
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ['error'],
-  });
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+export const prisma = new PrismaClient();

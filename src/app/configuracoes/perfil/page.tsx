1040f8d2a001dbc4db66925339d92077e5801450
borgesr18 <borgesr18@yahoo.com.br>
2025-06-15 21:46:04 -0300

    Update page.tsx

--- src/app/configuracoes/perfil/page.tsx
+++ src/app/configuracoes/perfil/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { useUsuarios } from '@/lib/useUsuarios';
+import { useAuth } from '@/lib/useAuth'
 import Input from '@/components/ui/Input';
 import Button from '@/components/ui/Button';
 

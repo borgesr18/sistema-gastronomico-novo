0a3a219acbe655edc7f4b23bde4537aa5d5b1960
borgesr18 <borgesr18@yahoo.com.br>
2025-06-17 16:01:01 -0300

    Update SlideOver.tsx

--- src/components/ui/SlideOver.tsx
+++ src/components/ui/SlideOver.tsx
@@ -1,16 +1,38 @@
+'use client';
 
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
 
 interface SlideOverProps {
+  isOpen: boolean;
   onClose: () => void;
+  title: string;
   children: React.ReactNode;
 }
 
-export default function SlideOver({ onClose, children }: SlideOverProps) {
+export default function SlideOver({ isOpen, onClose, title, children }: SlideOverProps) {
   return (
-    <div>
-      <div onClick={onClose}></div>
-      {children}
-    </div>
+    <Transition show={isOpen} as={Fragment}>
+      <Dialog as="div" className="fixed inset-0 overflow-hidden z-50" onClose={onClose}>
+        <div className="absolute inset-0 overflow-hidden">
+          <Transition.Child
+            as={Fragment}
+            enter="transform transition ease-in-out duration-500 sm:duration-700"
+            enterFrom="translate-x-full"
+            enterTo="translate-x-0"
+            leave="transform transition ease-in-out duration-500 sm:duration-700"
+            leaveFrom="translate-x-0"
+            leaveTo="translate-x-full"
+          >
+            <Dialog.Panel className="absolute right-0 w-screen max-w-md h-full bg-white shadow-xl">
+              <div className="p-6 border-b">
+                <Dialog.Title className="text-lg font-medium text-gray-900">{title}</Dialog.Title>
+              </div>
+              <div className="p-6 overflow-y-auto h-full">{children}</div>
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
   );
 }

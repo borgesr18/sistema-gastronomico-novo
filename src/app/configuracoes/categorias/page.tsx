b1c616245e6133e7d66dad708bce31306e70fb6a
borgesr18 <borgesr18@yahoo.com.br>
2025-06-17 08:59:16 +0000

    Corrige página

--- src/app/configuracoes/categorias/page.tsx
+++ src/app/configuracoes/categorias/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { Categoria } from '@prisma/client';
 import Button from '@/components/ui/Button';
 import Modal, { useModal } from '@/components/ui/Modal';
-import Table, { TableRow, TableCell } from '@/components/ui/Table';
+import { Table, TableRow, TableCell } from '@/components/ui/Table';
 import Toast from '@/components/ui/Toast';
 import Input from '@/components/ui/Input';
 import CategoriaForm from '../_components/CategoriaForm';
